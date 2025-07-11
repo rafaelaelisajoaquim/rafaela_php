b0bd536d7f3ca8abdf455e86e1dd9309fb0cd9cf
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-07-11 13:55:02 -0300

    Versão 1

--- combinacao_html.php
+++ combinacao_html.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Combinação</title>
 <body>
     <?php
     //Função usada para definir fuso horário padrão
--- saida.php
+++ saida.php
@@ -1,4 +1,9 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saída</title>
     <head>
         <title>Primeiro programa PHP</title>
     </head>
