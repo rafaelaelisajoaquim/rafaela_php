330d8a938e2fc79e1b16a5822d5b3a6cf1fdce77
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-07-18 15:30:26 -0300

    versão 1

--- funcao_texto/ignorar_texto.php
+++ funcao_texto/ignorar_texto.php
@@ -1,6 +1,6 @@
 <?php
 #file_ignore_new_lines ignorar o \n de cada linha
-    $linhas = file ("texto.txt", file_ignore_new_lines);
+    $linhas = file ("texto.txt", FILE_IGNORE_NEW_LINES);
     var_dump($linhas);
     foreach ($linhas as $linha_num => $linhas){
         echo "Linha #{$linha_num} : ".$linha."<br>";
