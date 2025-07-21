7d4cd02f67f3c99f5ab992cad8da19bcafa4ad7b
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-07-21 17:11:05 -0300

    Versão 1

--- sessao_cookies/banco.php
+++ sessao_cookies/banco.php
@@ -4,7 +4,7 @@
     $bdSenha = '';
     $bdBanco = 'rafaela_joaquim';
     $conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
-        if (mysqli_connect_errno($conexao)) {
+        if (mysqli_connect_errno()) {
             echo "Problemas para conectar com o banco. Verifique os dados.";
         die();
         }
