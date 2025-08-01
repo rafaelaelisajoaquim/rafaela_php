57ccfb2fd2226b5ba9e6ccd92fa3981838c6db5c
Rafaela Elisa Joaquim <rafaelaelisaj@gmail.com>
2025-08-01 15:33:32 -0300

    Versão 1

--- conexao_banco/conexao_mysql/conexao.php
+++ conexao_banco/conexao_mysql/conexao.php
@@ -1,6 +1,6 @@
 <?php 
     //Habilita relatório detalhado de erros no MySQLI
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    mysqli_report(MYSQLI_REPORT_ERROR|MYSQLI_REPORT_STRICT);
 
     /*Função para conectar no banco de dados 
      *Retorna um objeto de conexão MySQLI 
@@ -18,7 +18,8 @@
             $con = new mysqli($endereco,$usuario,$senha,$banco);
     
             //Definição de conjunto de caracteres para eviar problemas de acentuação
-            $con->set_charset("utf8mb4"); //Retorna o objeto de conexão
+            $con->set_charset("utf8mb4");
+            return $con; //Retorna o objeto de conexão
         } catch (Exception $e){
             //Exibe uma mensagem de erro e encerra o script
             die("Erro na conexão: ".$e->getMessage());
